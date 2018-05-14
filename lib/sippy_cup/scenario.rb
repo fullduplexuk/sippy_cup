4087e567525857995c74fdd7b8f64ba07566cb36
mala.sankara <mala.sankara@textnow.com>
2018-05-14 12:23:25 -0400

    Adding support to send 407 proxy authorization Adding support to send invite with authorization header passed via options hash.

--- lib/sippy_cup/scenario.rb
+++ lib/sippy_cup/scenario.rb
@@ -200,6 +200,62 @@ a=fmtp:101 0-15
       @reference_variables += %w(remote_addr local_addr call_addr)
     end
 
+
+    #
+    # Send an invite message with auth header
+    #
+    # @param [Hash] opts A set of options to modify the message
+    # @option opts [Integer] :retrans
+    # @option opts [String] :headers Extra headers to place into the INVITE
+    def invite_auth(opts = {})
+      #opts[:retrans] ||= 500
+      # FIXME: The DTMF mapping (101) is hard-coded. It would be better if we could
+      # get this from the DTMF payload generator
+      from_addr = "#{@from_user}@#{@adv_ip}:[local_port]"
+      msg = <<-AUTH
+INVITE sip:#{to_addr} SIP/2.0 
+Via: SIP/2.0/[transport] #{@adv_ip}:[local_port];branch=[branch]
+From: "#{@from_user}" <sip:#{from_addr}>;tag=[call_number]
+To: <sip:#{to_addr}>
+Call-ID: [call_id]
+CSeq: [cseq] INVITE
+Contact: <sip:#{from_addr};transport=[transport]>
+[#{opts[:auth_keyword]}]
+Max-Forwards: 100
+User-Agent: #{USER_AGENT}
+Content-Type: application/sdp
+Content-Length: [len]
+#{opts.has_key?(:headers) ? opts.delete(:headers).sub(/\n*\Z/, "\n") : ''}
+v=0
+o=user1 53655765 2353687637 IN IP[local_ip_type] [local_ip]
+s=-
+c=IN IP[media_ip_type] [media_ip]
+t=0 0
+m=audio [media_port] RTP/AVP 0 101
+a=rtpmap:0 PCMU/8000
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-15
+      AUTH
+      send msg,opts do |send|
+        send << doc.create_element('action') do |action|
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "remote_addr"
+            assignstr['value']     = to_addr
+          end
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "local_addr"
+            assignstr['value']     = from_addr
+          end
+          action << doc.create_element('assignstr') do |assignstr|
+            assignstr['assign_to'] = "call_addr"
+            assignstr['value']     = to_addr
+          end
+        end
+      end
+      # These variables will only be used if we initiate a hangup
+      @reference_variables += %w(remote_addr local_addr call_addr)
+    end
+
     #
     # Send a REGISTER message with the specified credentials
     #
@@ -364,6 +420,17 @@ a=rtpmap:0 PCMU/8000
       handle_response 100, opts
     end
     alias :receive_100 :receive_trying
+
+    #
+    # Sets an expectation for a SIP 407 message from the remote party
+    #
+    # @param [Hash] opts A set of options to modify the expectation
+    # @option opts [true, false] :optional Whether or not receipt of the message is optional. Defaults to true.
+    #
+    def proxy_auth(opts = {})
+      handle_response 407, opts
+    end
+    alias :receive_407 :proxy_auth
 
     #
     # Sets an expectation for a SIP 180 message from the remote party
